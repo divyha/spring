f83fe80c1accff4d02d097fc9f31ae5193dc2b9b
meshuaib <meshuaib1@gmail.com>
2018-02-16 17:57:02 +0530

    Update AppController.java

--- src/main/java/sample/AppController.java
+++ src/main/java/sample/AppController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-	@RequestMapping("/gettingStarted")
+	@RequestMapping("/gettingstarted")
 	public String gettingStarted()
 	{
-	return "Getting Started with Highlight PDF";
+	return "Getting Started with Spring";
 	}
 }
